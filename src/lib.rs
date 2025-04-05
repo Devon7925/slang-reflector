bfca10071f04931a0190f99716cb286e9fe65247
Devon7925 <devonrutledge03@gmail.com>
2025-04-04 23:50:52 -0600

    Various improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
-pub use slang::{
-    Blob, CompileTarget, CompilerOptions, ComponentType, Error, FileSystem, GlobalSession, Module,
-    OptimizationLevel, Result, ScalarType, Session, SessionDesc, Stage, TargetDesc,
-};
-use slang::{ImageFormat, ParameterCategory, TypeKind, reflection::UserAttribute};
+use std::collections::HashMap;
 
+pub use slang::{
+    Blob, CompileTarget, CompilerOptions, ComponentType, Error, FileSystem, GlobalSession,
+    ImageFormat, Module, OptimizationLevel, ResourceAccess, Result, ScalarType, Session,
+    SessionDesc, Stage, TargetDesc,
+};
+use slang::{ParameterCategory, TypeKind, reflection::UserAttribute};
+
+#[derive(Debug)]
 pub enum UserAttributeParameter {
     String(String),
     Int(i32),
     Float(f32),
 }
 
+#[derive(Debug)]
 pub struct UserAttributeReflection {
     pub name: String,
     pub parameters: Vec<UserAttributeParameter>,
 }
 
+#[derive(Debug)]
 pub enum TextureType {
     Dim1,
     Dim2,
@@ -22,40 +28,84 @@ pub enum TextureType {
     Cube,
 }
 
+#[derive(Debug)]
 pub struct VariableReflection {
     pub name: String,
-    pub reflection_type: BoundResource,
+    pub reflection_type: BoundParameter,
     pub user_attributes: Vec<UserAttributeReflection>,
 }
 
-pub enum BoundResource {
+#[derive(Debug)]
+pub enum BoundParameter {
     Uniform {
         uniform_offset: usize,
         resource_result: VariableReflectionType,
     },
-    StructuredBuffer(VariableReflectionType),
+    Resource {
+        resource: BoundResource,
+        binding_index: u32,
+    },
+}
+
+#[derive(Debug)]
+pub enum BoundResource {
+    StructuredBuffer {
+        resource_result: VariableReflectionType,
+        resource_access: slang::ResourceAccess,
+    },
     Sampler,
     Texture {
         tex_type: TextureType,
         resource_result: VariableReflectionType,
         format: ImageFormat,
+        resource_access: slang::ResourceAccess,
     },
 }
 
+#[derive(Debug)]
 pub enum VariableReflectionType {
     Struct(Vec<(String, VariableReflectionType)>),
     Scalar(slang::ScalarType),
     Vector(slang::ScalarType, usize),
 }
 
+fn get_scalar_size(scalar_type: &slang::ScalarType) -> u32 {
+    match scalar_type {
+        slang::ScalarType::Int8 | slang::ScalarType::Uint8 => 1,
+        slang::ScalarType::Int16 | slang::ScalarType::Uint16 | slang::ScalarType::Float16 => 2,
+        slang::ScalarType::Int32 | slang::ScalarType::Uint32 | slang::ScalarType::Float32 => 4,
+        slang::ScalarType::Int64 | slang::ScalarType::Uint64 | slang::ScalarType::Float64 => 8,
+        _ => panic!("Unrecognized scalar type"),
+    }
+}
+
+impl VariableReflectionType {
+    pub fn get_size(&self) -> u32 {
+        match self {
+            VariableReflectionType::Scalar(scalar_type) => get_scalar_size(scalar_type),
+            VariableReflectionType::Vector(scalar_type, count) => {
+                let count = count.next_power_of_two() as u32;
+                count * get_scalar_size(scalar_type)
+            }
+            VariableReflectionType::Struct(fields) => fields
+                .iter()
+                .map(|(_, field_data)| field_data.get_size())
+                .fold(0, |a, f| (a + f).div_ceil(f) * f),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct EntrypointReflection {
     pub name: String,
     pub user_attributes: Vec<UserAttributeReflection>,
 }
 
+#[derive(Debug)]
 pub struct ProgramReflection {
     pub variables: Vec<VariableReflection>,
     pub entry_points: Vec<EntrypointReflection>,
+    pub hashed_strings: HashMap<u32, String>,
 }
 
 pub trait ProgramLayoutReflector {
@@ -96,9 +146,15 @@ impl ProgramLayoutReflector for slang::ProgramLayout {
             })
         }
 
+        let hashed_strings = (0..self.hashed_string_count())
+            .map(|i| self.hashed_string(i).unwrap().to_string())
+            .map(|s| (slang::reflection::compute_string_hash(s.as_str()), s))
+            .collect();
+
         ProgramReflection {
             variables,
             entry_points,
+            hashed_strings,
         }
     }
 }
@@ -135,57 +191,57 @@ fn parameter_user_attributes<'a>(
 fn bound_resource_from_slang_type(
     slang_type: &slang::reflection::Type,
     slang_layout: &slang::reflection::VariableLayout,
-) -> BoundResource {
-    match slang_layout.category() {
-        slang::ParameterCategory::None => todo!(),
-        slang::ParameterCategory::Mixed => todo!(),
-        slang::ParameterCategory::ConstantBuffer => todo!(),
-        slang::ParameterCategory::ShaderResource => match slang_type.resource_shape() {
-            slang::ResourceShape::SlangResourceBaseShapeMask => todo!(),
-            slang::ResourceShape::SlangResourceNone => todo!(),
+) -> BoundParameter {
+    if matches!(slang_layout.category(), ParameterCategory::Uniform)  {
+        return BoundParameter::Uniform {
+            uniform_offset: slang_layout.offset(ParameterCategory::Uniform),
+            resource_result: reflection_type_from_slang_type(slang_type, Some(slang_layout)),
+        }
+    }
+    match slang_type.kind() {
+        slang::TypeKind::Resource => match slang_type.resource_shape() {
             slang::ResourceShape::SlangTexture1d
             | slang::ResourceShape::SlangTexture2d
             | slang::ResourceShape::SlangTexture3d
-            | slang::ResourceShape::SlangTextureCube => BoundResource::Texture {
-                tex_type: resource_shape_to_tex_type(slang_type.resource_shape()),
-                resource_result: reflection_type_from_slang_type(
-                    slang_type.resource_result_type(),
-                    Some(slang_layout.type_layout().element_var_layout()),
-                ),
-                format: slang_layout.image_format(),
+            | slang::ResourceShape::SlangTextureCube => BoundParameter::Resource {
+                binding_index: slang_layout.binding_index(),
+                resource: BoundResource::Texture {
+                    tex_type: resource_shape_to_tex_type(slang_type.resource_shape()),
+                    resource_result: reflection_type_from_slang_type(
+                        slang_type.resource_result_type(),
+                        Some(slang_layout.type_layout().element_var_layout()),
+                    ),
+                    format: slang_layout.image_format(),
+                    resource_access: slang_layout.type_layout().resource_access().unwrap(),
+                },
             },
-            slang::ResourceShape::SlangTextureBuffer => todo!(),
-            slang::ResourceShape::SlangStructuredBuffer => {
-                BoundResource::StructuredBuffer(reflection_type_from_slang_type(
-                    slang_type.element_type(),
-                    Some(slang_layout.type_layout().element_var_layout()),
-                ))
-            }
-            slang::ResourceShape::SlangByteAddressBuffer => todo!(),
-            slang::ResourceShape::SlangResourceUnknown => todo!(),
-            slang::ResourceShape::SlangAccelerationStructure => todo!(),
-            slang::ResourceShape::SlangTextureSubpass => todo!(),
-            slang::ResourceShape::SlangResourceExtShapeMask => todo!(),
-            slang::ResourceShape::SlangTextureFeedbackFlag => todo!(),
-            slang::ResourceShape::SlangTextureShadowFlag => todo!(),
-            slang::ResourceShape::SlangTextureArrayFlag => todo!(),
-            slang::ResourceShape::SlangTextureMultisampleFlag => todo!(),
-            slang::ResourceShape::SlangTexture1dArray => todo!(),
-            slang::ResourceShape::SlangTexture2dArray => todo!(),
-            slang::ResourceShape::SlangTextureCubeArray => todo!(),
-            slang::ResourceShape::SlangTexture2dMultisample => todo!(),
-            slang::ResourceShape::SlangTexture2dMultisampleArray => todo!(),
-            slang::ResourceShape::SlangTextureSubpassMultisample => todo!(),
+            slang::ResourceShape::SlangStructuredBuffer => BoundParameter::Resource {
+                binding_index: slang_layout.binding_index(),
+                resource: BoundResource::StructuredBuffer {
+                    resource_result: reflection_type_from_slang_type(
+                        slang_type.element_type(),
+                        Some(slang_layout.type_layout().element_var_layout()),
+                    ),
+                    resource_access: slang_layout.type_layout().resource_access().unwrap(),
+                },
+            },
+            slang::ResourceShape::SlangResourceNone => panic!(
+                "{:?}, {:?}, {:?}, {:?}",
+                slang_layout.category(),
+                slang_type.resource_shape(),
+                slang_type.name(),
+                slang_type.kind()
+            ),
+            rs => panic!("{rs:?} resource shape not implemented for bound_resource_from_slang_type")
         },
-        slang::ParameterCategory::UnorderedAccess => todo!(),
-        slang::ParameterCategory::VaryingInput => todo!(),
-        slang::ParameterCategory::VaryingOutput => todo!(),
-        slang::ParameterCategory::SamplerState => BoundResource::Sampler,
-        slang::ParameterCategory::Uniform => BoundResource::Uniform {
-            uniform_offset: slang_layout.offset(ParameterCategory::Uniform),
-            resource_result: reflection_type_from_slang_type(slang_type, Some(slang_layout)),
+        slang::TypeKind::SamplerState => BoundParameter::Resource {
+            binding_index: slang_layout.binding_index(),
+            resource: BoundResource::Sampler,
         },
-        _ => panic!("Not recognized as valid top level type category"),
+        ty => panic!(
+            "{ty:?} not recognized as valid top level type category for object of type {:?}",
+            slang_type.kind()
+        ),
     }
 }
 
